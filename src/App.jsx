a03bac66c5366cf6ceb4c339262ea99a709eebec
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2024-01-23 22:17:56 +0600

    Ui is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import TaskBoard from "./components/Task/TaskBoard";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Hero></Hero>
+      <TaskBoard></TaskBoard>
+      <Footer></Footer>
     </>
   );
 }
