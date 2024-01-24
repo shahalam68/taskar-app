e4407f2999e6014718722ad2c967444739e9a830
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2024-01-24 18:24:07 +0600

    Add task modal created

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    descriptions:
+      " I want to learn react such then i can treat it like a slave and make it do whatever i want to do ",
+    tags: ["web", "react", "js"],
+    priority: "high",
+    isFavorite: false,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  const handleAddTask = () => {
+    console.log("Clicked");
+    setShowAddModal(true);
+  };
+
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal />}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask> </SearchTask>
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction />
-          <TaskList />
+          <TaskAction onAddClick={() => setShowAddModal(true)} />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
